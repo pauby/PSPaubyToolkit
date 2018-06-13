14235a51aea3fb25d6ab89ee3b0b15f5ee9b4e50
Paul Broadwith <pauby@users.noreply.github.com>
2018-06-13 22:32:52 +0100

    restructured layout

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # PSPaubyToolkit
 
 A repository for a collection of PowerShell scripts I create or ... borrow.
+
+This doesn't get a lot of love so feel free to give it some.
---
+++ source/public/Get-OS.ps1
@@ -0,0 +1,38 @@
+
+function Get-OS {
+
+    Param (
+        [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]$ComputerName = "localhost"
+    )
+
+    Begin {}
+
+    Process {
+        try {
+            # See https://msdn.microsoft.com/en-us/library/aa394239(v=vs.85).aspx for Win32_OperatingSystem breakdown
+            $osData = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $ComputerName
+        }
+        catch {
+            throw $Error[0]
+        }
+
+        @{
+            name = $osData.Caption;
+            architecture = $osData.OSArchitecture;
+            platform = switch ($osData.ProductType) { 
+                1 { "Workstation" }
+                2 { "Domain Controller" } 
+                3 { "Server" }
+            }
+            type = switch ($osData.OSType) { 
+                18 { "Windows" } 
+            }
+            version = $osData.Version;
+            buildnumber = $osData.BuildNumber;
+         }
+    }
+
+    End {}
+}
+
